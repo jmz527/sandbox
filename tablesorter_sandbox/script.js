88f24adf283bf8d7946e8906eaeecfb24172f370
James Rutledge <james.rutledge@EM10021.local>
2015-12-28 19:36:41 -0500

    added pagination, recreated pagecount issue

--- tablesorter_sandbox/script.js
+++ tablesorter_sandbox/script.js
@@ -1,7 +1,6 @@
 $(function() {
 	console.log('...Document Ready...');
 
-
 	$.tablesorter.addWidget({
 		id: "tableFootTotals",
 		priority: 1,
@@ -20,6 +19,43 @@ $(function() {
 			$('#table').find('tfoot tr td:nth-child(5)').html((total-totalFiltered));
 		}
 
+	});
+
+	// **********************************
+	//  Description of ALL pager options
+	// **********************************
+	var pagerOptions = {
+		// target the pager markup - see the HTML block below
+		container: $('#pager'),
+		size: 10,
+		// output string - default is '{page}/{totalPages}'
+		// possible variables: {size}, {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+		// also {page:input} & {startRow:input} will add a modifiable input in place of the value
+		output: 'Showing {startRow} - {endRow} of {totalRows} entries',
+		// if true, the table will remain the same height no matter how many records are displayed. The space is made up by an empty
+		// table row set to a height to compensate; default is false
+		fixedHeight: false,
+		// remove rows from the table to speed up the sort of large tables.
+		// setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+		removeRows: false,
+		// go to page selector - select dropdown that sets the current page
+		cssGoto: '.gotoPage'
+	};
+	// **********************************
+	//  Customized pager options
+	// **********************************
+	$.tablesorter.customPagerControls({
+		table: $table,
+		pager: $('#pager'),
+		pageSize: '.left a',
+		currentPage: '.right a',
+		ends: 2,
+		aroundCurrent: 1,
+		link: '<li><a href="#">{page}</a></li>',
+		currentClass: 'current',
+		adjacentSpacer: '',
+		distanceSpacer: '',
+		addKeyboard: true
 	});
 
 	var $table = $('#table').tablesorter({
@@ -41,6 +77,8 @@ $(function() {
 			filter_saveFilters : false,
 			filter_reset: '.reset'
 		}
-	});
-
+	})
+	// initialize the pager plugin
+	// ****************************
+	.tablesorterPager(pagerOptions);
 });
