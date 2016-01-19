d159d82b33cec5167c2418b25591771ac7d2aabb
James Rutledge <james.rutledge@EM10021.local>
2016-01-19 16:10:04 -0500

    jquery alert on about page

--- require_js_sandbox/node_install/index.js
+++ require_js_sandbox/node_install/index.js
@@ -8,36 +8,6 @@ var requirejs = require('requirejs');
 app.use('/js', express.static(path.join(__dirname, 'assets/js')));
 app.use('/modules', express.static(path.join(__dirname, 'node_modules')));
 
-/**
- * The first JS file to be loaded. Takes care of setting up all of
- * the required paths
- */
-
- // Configure RequireJS
- requirejs.config({
- 	baseUrl: '/js',
- 	paths: {
- 		// The libraries we use
- 		jquery: [
- 			'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min',
- 			//If the CDN location fails, load from this location
- 			'https://code.jquery.com/jquery-2.2.0.min.js'
- 		],
- 		bootstrap: [
- 			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
- 			//If the CDN location fails, load from this location
- 			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min'
- 		]
- 	}
- });
-
-// requirejs.config({
-//     //Pass the top-level main.js/index.js require
-//     //function to requirejs so that node modules
-//     //are loaded relative to the top-level JS file.
-//     nodeRequire: require
-// });
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
