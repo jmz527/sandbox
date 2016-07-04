546de241b7ae555cec524d9431567d758a0ecaf6
James Rutledge <jarutledge2@gmail.com>
2016-07-04 04:51:41 -0400

    more nodebot stuff

--- nodebots/servo.js
+++ nodebots/servo.js
@@ -3,8 +3,12 @@ var board = new five.Board();
 
 board.on("ready", function() {
 
-  var servo = new five.Servo(11);
+  // Create a new `servo` hardware instance.
+  var servo = new five.Servo.Continuous(10);
 
+  // Inject the `servo` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
   this.repl.inject({
     servo: servo
   });
