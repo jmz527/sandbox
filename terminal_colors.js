e20e263b04f374f0d21075e242b9cbe419f32460
James Rutledge <jarutledge2@gmail.com>
2017-08-18 19:15:38 -0400

    console log colors

---
+++ terminal_colors.js
@@ -0,0 +1,63 @@
+
+// console.log("\x1b[0m", "Reset")
+// console.log("\x1b[1m", "Bright")
+// console.log("\x1b[2m", "Dim")
+// console.log("\x1b[4m", "Underscore")
+// console.log("\x1b[5m", "Blink")
+// console.log("\x1b[7m", "Reverse")
+// console.log("\x1b[8m", "Hidden")
+
+// console.log("\x1b[30m", "FgBlack")
+// console.log("\x1b[31m", "FgRed")
+// console.log("\x1b[32m", "FgGreen")
+// console.log("\x1b[33m", "FgYellow")
+// console.log("\x1b[34m", "FgBlue")
+// console.log("\x1b[35m", "FgMagenta")
+// console.log("\x1b[36m", "FgCyan")
+// console.log("\x1b[37m", "FgWhite")
+
+// console.log("\x1b[40m", "BgBlack")
+// console.log("\x1b[41m", "BgRed")
+// console.log("\x1b[42m", "BgGreen")
+// console.log("\x1b[43m", "BgYellow")
+// console.log("\x1b[44m", "BgBlue")
+// console.log("\x1b[45m", "BgMagenta")
+// console.log("\x1b[46m", "BgCyan")
+// console.log("\x1b[47m", "BgWhite")
+
+
+console.log('|cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc|')
+
+
+console.log('\x1b[30m%s\x1b[0m', 'I am black'); //black
+console.log('\x1b[31m%s\x1b[0m', 'I am red');  //red
+console.log('\x1b[32m%s\x1b[0m', 'I am green'); //green
+console.log('\x1b[33m%s\x1b[0m', 'I am yellow');  //yellow
+console.log('\x1b[34m%s\x1b[0m', 'I am blue'); //blue
+console.log('\x1b[35m%s\x1b[0m', 'I am violet');  //color
+console.log('\x1b[36m%s\x1b[0m', 'I am cyan');  //cyan
+
+
+console.log('|cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc|')
+
+
+// console.log('\x1b[30m%s\x1b[0m', 'I am a color 30')
+// console.log('\x1b[31m%s\x1b[0m', 'I am a color 31')
+// console.log('\x1b[32m%s\x1b[0m', 'I am a color 32')
+// console.log('\x1b[33m%s\x1b[0m', 'I am a color 33')
+// console.log('\x1b[34m%s\x1b[0m', 'I am a color 34')
+// console.log('\x1b[35m%s\x1b[0m', 'I am a color 35')
+// console.log('\x1b[36m%s\x1b[0m', 'I am a color 36')
+
+// console.log('|cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc|')
+
+// console.log('\x1b[41m%s\x1b[0m', 'I am a color 41')
+// console.log('\x1b[42m%s\x1b[0m', 'I am a color 42')
+// console.log('\x1b[43m%s\x1b[0m', 'I am a color 43')
+// console.log('\x1b[44m%s\x1b[0m', 'I am a color 44')
+// console.log('\x1b[45m%s\x1b[0m', 'I am a color 45')
+// console.log('\x1b[46m%s\x1b[0m', 'I am a color 46')
+// console.log('\x1b[47m%s\x1b[0m', 'I am a color 47')
+
+
+// console.log('|cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc|')
